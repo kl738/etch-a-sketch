7818906bb424dc9ab254c40b6e2ae7eebc9cfa5c
kevin <kl738@cornell.edu>
2018-04-27 15:10:22 -0400

    did command

---
+++ command.ml
@@ -0,0 +1,26 @@
+type command =
+  | New
+  | Open of string
+  | Save of string
+  | Quit
+
+(* [get_first_word] Gets the first word of a string by using space *)
+let get_first_word str =
+    String.split_on_char ' ' (String.trim str) |> List.hd
+
+let parse str =
+  match str |> String.lowercase_ascii |> get_first_word with
+  | "new" -> New
+  | "quit" -> Quit
+  | "save" ->
+    let str_lst =   String.split_on_char ' ' (String.trim str) in
+    if List.length str_lst > 2 then failwith "Too many arguments"
+    else if List.length str_lst = 1 then failwith "Please specify a file name"
+    else let file_name = List.nth (String.split_on_char ' ' str) 1 in
+    Save(file_name)
+  | "open" ->   let str_lst =   String.split_on_char ' ' (String.trim str) in
+    if List.length str_lst > 2 then failwith "Too many arguments"
+    else if List.length str_lst = 1 then failwith "Please specify a file name"
+    else let file_name = List.nth (String.split_on_char ' ' str) 1 in
+    Open(file_name)
+  | _ -> failwith "Unrecognized command"
