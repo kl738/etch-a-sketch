64354d66aba8fb45effd371585c043c15e54f20d
Jack Wilson Thompson <jwt84@cornell.edu>
2018-05-14 11:59:36 -0400

    image converter beta

--- image_converter.ml
+++ image_converter.ml
@@ -1,6 +1,6 @@
-open State
+(* open State *)
 (* can't test in utop without doing #use "state.ml" etc. *)
-
+type pt = int * int
 (** [find_root a x y] traverses [a] starting at position ([x], [y])
   *    to find the first black pixel and return its position.   *)
 let rec find_root (a: int array array) x y =
@@ -13,32 +13,65 @@ let rec find_root (a: int array array) x y =
 
 type pix = {use : bool; c : (int)}
 
+
+(* necessary to avoid circular pts but need to mark pts as seen in traversal, cant within ifs lol *)
 let map_seen a =
   Array.map (fun b -> Array.map (fun x -> {use = false; c = x}) b) a
 
-let rec group_pixels a lst : (int * int) list =
-  match lst with
-  | [] -> group_pixels a ((find_root a 0 0) :: [])
-  | h::t -> match h with
-    | (x, y) -> if (x == Array.length a - 1) && (y == Array.length a.(x) - 1)
-                  then lst
-                else if a.(x).(y+1) == 0 && not (List.mem ((x),(y+1)) lst)
-                  then group_pixels a ((x, y+1) :: lst)
-                else if a.(x+1).(y) == 0 && not (List.mem ((x+1),(y)) lst)
-                  then group_pixels a ((x+1, y) :: lst)
-                else if a.(x).(y-1) == 0 && not (List.mem ((x),(y-1)) lst)
-                  then group_pixels a ((x, y-1) :: lst)
-                else if a.(x-1).(y) == 0 && not (List.mem ((x-1),(y)) lst)
-                  then group_pixels a ((x-1, y) :: lst)
-                else lst
+(** [pt tree] is a tree that will be used to group all pixels together
+  *  format of a node is pt, Up, Down, Left, Right *)
+type 'a tree =
+  | Leaf
+  | Node of 'a * 'a tree * 'a tree * 'a tree * 'a tree
 
-let rec pts_to_segs pts segs =
+(* let check_pix a dir (x,y) = a.(x).(y) <- {a.(x).(y) with use = true};
+  match dir with
+  | Up -> if a.(x-1).(y) == 0 && not (List.mem ((x-1),(y)) lst)
+                  then group_pixels a ((x-1, y) :: lst)
+
+  | Down -> if a.(x+1).(y) == 0 && (x < Array.length a - 1) && not (List.mem ((x+1),(y)) lst)
+                  then group_pixels a ((x+1, y) :: lst)
+  | Left -> if a.(x).(y-1) == 0 && not (List.mem ((x),(y-1)) lst)
+                  then group_pixels a ((x, y-1) :: lst)
+  | Right -> if a.(x).(y+1) == 0 && (y < Array.length a.(x) - 1) && not (List.mem ((x),(y+1)) lst)
+                  then group_pixels a ((x, y+1) :: lst)
+  else lst *)
+
+
+let set_use (x,y) a =
+  a.(x).(y) <- {a.(x).(y) with use = true}
+
+
+let rec group_pixels root a : pt tree =
+  let x = fst root in let y = snd root in
+  if x >= Array.length a || y >= Array.length a.(x) then Leaf
+  else if a.(x).(y).c == 0 && not a.(x).(y).use then let _ = set_use (x,y) a in
+      Node ((fst root, snd root),
+        (group_pixels (x-1,y) a),
+        (group_pixels (x+1,y) a),
+        (group_pixels (x,y-1) a),
+        (group_pixels (x,y+1) a))
+  else Leaf
+
+
+let rec dfs tree =
+  match tree with
+  | Leaf -> []
+  | Node (rt, u, d, l, r) -> [rt]@(dfs l)@[rt]@(dfs d)@[rt]@(dfs r)@[rt]@(dfs u)@[rt]
+
+let rec pts_to_segs pts segs = let def_seg = {
+  direction= Up;
+  length= 1;
+  color= "00000000";
+  width= 1;
+  opacity= 1.0;
+} in
   match pts with
   | [] -> segs
   | p1 :: p2 :: t -> begin match (fst p1 - fst p2, snd p1 - snd p2) with
-      | (1, 0) -> pts_to_segs (p2::t) (Down :: segs)
-      | (0, 1) -> pts_to_segs (p2::t) (Right :: segs)
-      | (-1, 0) -> pts_to_segs (p2::t) (Up :: segs)
-      | (0, -1) -> pts_to_segs (p2::t) (Left :: segs)
+      | (1, 0) -> pts_to_segs (p2::t) ({def_seg with direction = Down} :: segs)
+      | (0, 1) -> pts_to_segs (p2::t) ({def_seg with direction = Right} :: segs)
+      | (-1, 0) -> pts_to_segs (p2::t) ({def_seg with direction = Up} :: segs)
+      | (0, -1) -> pts_to_segs (p2::t) ({def_seg with direction = Left} :: segs)
       | _ -> pts_to_segs (p2::t) (segs) end
   | _ -> segs
