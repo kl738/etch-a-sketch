018b0a52c561b7805f97908e4898362d7147eb27
Jack Wilson Thompson <jwt84@cornell.edu>
2018-05-15 15:35:53 -0400

    got rid of array_of_image

--- view.ml
+++ view.ml
@@ -10,6 +10,7 @@ open Graphics
 open Camlimages
 open Images
 open Png
+open Graphic_image
 
 (***** BEGIN CONSTANTS *****)
 
@@ -69,41 +70,6 @@ let rec slow_draw_segs segs rt=
     | h::t -> draw_segment h; Unix.sleepf 0.005; draw_segs t
 
 let update_display st x y = moveto x y; draw_segs st.segments
-
-(*TODO: resize image and canvas to specific canvas size*)
-(* let resize img w h =
-  failwith "unimplemented" *)
-
-(** [array_of_image img] converts [img] to a color array
-  * requires: [img] is RGB or Index *)
-let array_of_image img =
-  match img with
-  | Images.Index8 bitmap ->
-      let w = bitmap.Index8.width
-      and h = bitmap.Index8.height
-      and colormap = bitmap.Index8.colormap.map in
-      let cmap = Array.map (fun {r = r; g = g; b = b} -> Graphics.rgb r g b) colormap in
-      if bitmap.Index8.transparent <> -1 then
-        cmap.(bitmap.Index8.transparent) <- transp;
-      Array.init h (fun i ->
-        Array.init w (fun j -> cmap.(Index8.unsafe_get bitmap j i)))
-  | Index16 bitmap ->
-      let w = bitmap.Index16.width
-      and h = bitmap.Index16.height
-      and colormap = bitmap.Index16.colormap.map in
-      let cmap = Array.map (fun {r = r; g = g; b = b} -> rgb r g b) colormap in
-      if bitmap.Index16.transparent <> -1 then
-        cmap.(bitmap.Index16.transparent) <- transp;
-      Array.init h (fun i ->
-        Array.init w (fun j -> cmap.(Index16.unsafe_get bitmap j i)))
-  | Rgb24 bitmap ->
-      let w = bitmap.Rgb24.width
-      and h = bitmap.Rgb24.height in
-      Array.init h (fun i ->
-        Array.init w (fun j ->
-          let {r = r; g = g; b = b} = Rgb24.unsafe_get bitmap j i in
-          rgb r g b))
-  | Rgba32 _ | Cmyk32 _ -> failwith "RGBA and CMYK not supported"
 
 (** [draw_image img x y] draws [img] on the canvas starting at point [x],[y] *)
 let draw_image img x y =
